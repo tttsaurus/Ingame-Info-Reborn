89d8b2c0f9571be8cb2e68fbf649031e9cdaa33b
tttsaurus <chenh1231y@gmail.com>
2025-01-30 01:46:13 -0500

    Safer texture bind

--- src/main/java/com/tttsaurus/ingameinfo/common/api/render/Texture2D.java
+++ src/main/java/com/tttsaurus/ingameinfo/common/api/render/Texture2D.java
@@ -1,10 +1,15 @@
 package com.tttsaurus.ingameinfo.common.api.render;
 
+import net.minecraft.client.renderer.GlStateManager;
 import org.lwjgl.opengl.GL11;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
 
 public class Texture2D
 {
+    private static final IntBuffer intBuffer = ByteBuffer.allocateDirect(16 << 2).order(ByteOrder.nativeOrder()).asIntBuffer();
+
     private int glTextureID = 0;
     private final int width;
     private final int height;
@@ -21,6 +26,9 @@ public class Texture2D
         this.width = width;
         this.height = height;
 
+        GL11.glGetInteger(GL11.GL_TEXTURE_BINDING_2D, intBuffer);
+        int textureID = intBuffer.get(0);
+
         GL11.glBindTexture(GL11.GL_TEXTURE_2D, glTextureID);
 
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR);
@@ -31,6 +39,8 @@ public class Texture2D
         GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_RGBA8, width, height, 0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE, byteBuffer);
 
         isGlBound = true;
+
+        GlStateManager.bindTexture(textureID);
     }
 
     public void dispose()
