a2ec8cf92fd1bd339fe085c67a18e5b289028f8d
tttsaurus <chenh1231y@gmail.com>
2025-03-23 21:26:53 -0400

    Updated mesh renderer

--- src/main/java/com/tttsaurus/ingameinfo/common/impl/render/renderer/MeshRenderer.java
+++ src/main/java/com/tttsaurus/ingameinfo/common/impl/render/renderer/MeshRenderer.java
@@ -2,12 +2,30 @@ package com.tttsaurus.ingameinfo.common.impl.render.renderer;
 
 import com.tttsaurus.ingameinfo.common.api.render.renderer.IRenderer;
 import com.tttsaurus.ingameinfo.common.impl.render.mesh.Mesh;
+import com.tttsaurus.saurus3d_snippet.common.api.shader.Shader;
 import com.tttsaurus.saurus3d_snippet.common.api.shader.ShaderProgram;
+import com.tttsaurus.saurus3d_snippet.common.impl.shader.ShaderLoader;
 
 public class MeshRenderer implements IRenderer
 {
-    private Mesh mesh = null;
-    private ShaderProgram shaderProgram = null;
+    public static ShaderProgram SHARED_MESH_SHADER_PROGRAM;
+    static
+    {
+        ShaderLoader loader = new ShaderLoader();
+        Shader frag = loader.load("ingameinfo:shaders/mesh_frag.glsl", Shader.ShaderType.FRAGMENT);
+        Shader vertex = loader.load("ingameinfo:shaders/mesh_vertex.glsl", Shader.ShaderType.VERTEX);
+        SHARED_MESH_SHADER_PROGRAM = new ShaderProgram(frag, vertex);
+        SHARED_MESH_SHADER_PROGRAM.setup();
+    }
+
+    public Mesh mesh;
+    public ShaderProgram shaderProgram;
+
+    public MeshRenderer(Mesh mesh, ShaderProgram shaderProgram)
+    {
+        this.mesh = mesh;
+        this.shaderProgram = shaderProgram;
+    }
 
     //<editor-fold desc="getters & setters">
     public void setMesh(Mesh mesh) { this.mesh = mesh; }
