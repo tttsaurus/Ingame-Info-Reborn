81893d785af8e485d841b1af9705043f92f67ed8
tttsaurus <chenh1231y@gmail.com>
2025-06-08 12:45:22 +0800

    Revert

--- src/main/java/com/tttsaurus/ingameinfo/nativeinterop/win/WindowsChecker.java
+++
@@ -1,9 +0,0 @@
-package com.tttsaurus.ingameinfo.nativeinterop.win;
-
-public class WindowsChecker
-{
-    public static boolean isWindows()
-    {
-        return System.getProperty("os.name").toLowerCase().contains("windows");
-    }
-}
