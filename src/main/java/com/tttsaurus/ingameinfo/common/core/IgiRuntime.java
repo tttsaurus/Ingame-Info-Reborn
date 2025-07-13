c0e44825adf7035f32d1142fa4363c69b5235767
tttsaurus <72607439+tttsaurus@users.noreply.github.com>
2025-07-13 13:16:56 +0800

    Bump to v1.2.0!

--- src/main/java/com/tttsaurus/ingameinfo/common/core/IgiRuntime.java
+++ src/main/java/com/tttsaurus/ingameinfo/common/core/IgiRuntime.java
@@ -122,10 +122,14 @@ public final class IgiRuntime
         public GlobalEntry openGuiOnStartup(String holderName, String mvvmRegistryName)
         {
             GuiLifecycleHolder holder = null;
-            if (holderName.equals(DefaultLifecycleHolder.HOLDER_NAME)) holder = lifecycleHolder;
-            for (GuiLifecycleHolder otherHolder: externaLifecycleHolders)
+            if (holderName.equals(DefaultLifecycleHolder.HOLDER_NAME))
+                holder = lifecycleHolder;
+            else for (GuiLifecycleHolder otherHolder: externaLifecycleHolders)
                 if (holderName.equals(otherHolder.getHolderName()))
+                {
                     holder = otherHolder;
+                    break;
+                }
 
             if (holder == null) return this;
 
